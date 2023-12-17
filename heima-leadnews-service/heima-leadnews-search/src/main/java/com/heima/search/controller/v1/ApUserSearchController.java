af1e42864f3f1c0c984b79be120334346cacefe8
aowang <15256403512@163.com>
2023-12-17 20:34:37 +0800

    feat: Day7-16完成：删除搜索记录功能

--- heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/controller/v1/ApUserSearchController.java
+++ heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/controller/v1/ApUserSearchController.java
@@ -1,10 +1,12 @@
 package com.heima.search.controller.v1;
 
 import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.search.dtos.HistorySearchDto;
 import com.heima.search.service.ApUserSearchService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,4 +30,8 @@ public class ApUserSearchController {
         return apUserSearchService.findUserSearch();
     }
 
+    @PostMapping("/del")
+    public ResponseResult delUserSearch(@RequestBody HistorySearchDto historySearchDto) {
+        return apUserSearchService.delUserSearch(historySearchDto);
+    }
 }
