135277167c300efde20fbc3d0d1e3d3e40e4ac0f
aowang <15256403512@163.com>
2023-12-17 21:55:36 +0800

    feat: Day8实战：后台管理登录功能

---
+++ heima-leadnews-gateway/heima-leadnews-admin-gateway/src/main/java/com/heima/admin/gateway/filter/AuthorizeFilter.java
@@ -0,0 +1,78 @@
+package com.heima.admin.gateway.filter;
+
+import com.heima.admin.gateway.util.AppJwtUtil;
+import io.jsonwebtoken.Claims;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class AuthorizeFilter implements Ordered, GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        // 1. 获取 request response
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
+
+        // 2. 判断是否是登录
+        if (request.getURI().getPath().contains("/login")) {
+            // 放行
+            return chain.filter(exchange);
+        }
+
+        // 3. 获取 token
+        String token = request.getHeaders().getFirst("token");
+        
+        // 4. 判断 token 是否存在 是否有效
+        if (StringUtils.isBlank(token)) {
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            response.setComplete();
+        }
+
+        // 5. 判断 token 是否有效
+        try {
+            Claims claimsBody = AppJwtUtil.getClaimsBody(token);
+            int result = AppJwtUtil.verifyToken(claimsBody);
+            if (result == 1 || result == 2) {
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                response.setComplete();
+            }
+
+            //获取用户信息
+            Object userId = claimsBody.get("id");
+
+            //存储header中
+            ServerHttpRequest serverHttpRequest = request.mutate().headers(httpHeaders -> {
+                httpHeaders.add("userId", userId + "");
+            }).build();
+            //重置请求
+            exchange.mutate().request(serverHttpRequest);
+
+        } catch (Exception e) {
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            response.setComplete();
+        }
+
+        // 6. 放行
+        return chain.filter(exchange);
+    }
+
+    /**
+     * 优先级设置 值越小 优先级越高
+     *
+     * @return
+     */
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
